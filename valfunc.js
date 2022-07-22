82ac7c6852bd23dca7db1c42346accda0ce355a7
Awfulot <Mr.Snisken1@gmail.com>
2022-07-22 14:13:45 -0400

    Added email validation and deleted tailwind config file

--- valfunc.js
+++ valfunc.js
@@ -1,4 +1,8 @@
-
+var hasLowercase = false;
+var hasUppercase = false;
+var hasNumber = false;
+var hasMinCharacters = false;
+var hasEmail = false;
 
 
 function changeColor(){
@@ -14,35 +18,69 @@ function validatePass() {
     //Check Password Length and change color accordingly
     if(pass.value.length <= 8) {
         document.getElementById("length").style.color ="red";
-        
+        hasMinCharacters = false;
         
         }
     else {
         document.getElementById("length").style.color = "green";
+        hasMinCharacters = true;
     }
     //Check if lowercase letters are present and change color accordingly
     if(pass.value.match(lowerCaseLetters)) {
         document.getElementById("lowerCaseLetter").style.color = "green";
+        hasLowercase = true;
         
     }
     else {
         document.getElementById("lowerCaseLetter").style.color = "red";
+        hasLowercase = false;
     }
     //Check if uppercase letters are present and change color accordingly
     if(pass.value.match(upperCaseLetters)) {
         document.getElementById("capital").style.color = "green";
+        hasUppercase = true;
         
     }
     else {
         document.getElementById("capital").style.color = "red";
+        hasUppercase = false;
     }
     //Check if numbers are present and change color accordingly
     if(pass.value.match(numbers)) {
         document.getElementById("numberAmount").style.color = "green";
+        hasNumber = true;
     }
     else {
         document.getElementById("numberAmount").style.color = "red";
+        hasNumber = false;
     }
 
    
+}
+function emailValidate() {
+    var email = document.getElementById("userEmail");
+    var atCharacter = /[@]/g;
+    var dotCharacter = /[.]/g;
+    var emailSymbols = /\S+@\S+\.\S+/g;
+
+    if(email.value.match(emailSymbols)) {
+        document.getElementById("emailCheck").style.color = "green";
+
+    }
+    else {
+        document.getElementById("emailCheck").style.color = "red";
+    }
+}
+
+
+
+function validationStatus() {
+    if(hasNumber && hasUppercase && hasLowercase && hasMinCharacters && hasEmail){
+        document.getElementById("validateCheck").innerHTML = "SUCCESS";
+        hasEmail = true;
+    }
+    else {
+        document.getElementById("validateCheck").innerHTML = "FAILURE";
+        hasEmail = false;
+    }
 }
